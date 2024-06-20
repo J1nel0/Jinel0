9e5191679caf0eedac826202f3f9e606ecf3692c
J1nel0 <gabrielps4jf@gmail.com>
2024-06-20 16:52:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var a, b, res
+a = parseInt(prompt("digite o valor de a "))
+b = parseInt(prompt("digite o valor de b"))
+res = a + b
+if (res == 10) {
+    document.write("legal")
+} else document.write("voce é gay")
